647a043780368a617ca73c4835ddc80b2e8f2490
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-08-09 16:39:24 +0100

    creating the Logo component

---
+++ src/Components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <h1 className=' text-white  text-4xl'> <span className='text-orange-600'>T</span><span className=' hidden md:inline'>ournify</span></h1>
+    )
+}
+
+export default Logo
