44c67919d0887172d1a43e75ed0da1efee9e6ec3
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-08-09 17:35:23 +0100

    creating the nav bar

---
+++ src/Components/Header/Navigation/Links/Links.tsx
@@ -0,0 +1,27 @@
+import Link from "./Link/Link"
+
+const Links = () => {
+    return (
+        <ul className="flex justify-around items-center">
+            <li>
+                <Link path="/" icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>} text="Home" />
+            </li>
+            <li>
+                <Link path="/messages" icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" />
+                    <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" />
+                </svg>}
+                    text="Inbox" />
+            </li>
+            <li>
+                <Link path="/notifications" icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                </svg>} text="Notifications" />
+            </li>
+        </ul>
+    )
+}
+
+export default Links
---
+++ src/Components/Header/Navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import Links from "./Links/Links"
+
+const Navigation = () => {
+    return (
+        <nav className=" w-60 ">
+            <Links />
+        </nav>
+    )
+}
+
+export default Navigation
